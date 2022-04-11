ccf60b838c1bf9e76a6fdb1d54e12f6a8e971caf
dmifed <dmitriy_fedorov@list.ru>
2022-04-11 17:04:22 +0300

    11 04 22 create pair map

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,22 @@
 import java.io.File;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
 
 /**
  * @author dmifed
  */
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception{
         String rootPath = "X:\\_project\\Петрович\\Таблички\\2021\\08 11 21 Панели Сириус\\crop_pic";
-        PathFinder pathFinder = new PathFinder(rootPath);
+        File root = new File(rootPath);
+        PathFinder pathFinder = new PathFinder(root);
 
-        pathFinder.findPathsAndNames(pathFinder.getRoot());
-        pathFinder.saveList(pathFinder.getRoot());
-        pathFinder.savePairsList(pathFinder.getRoot());
+        Map<String, List<String>> mapOfListFilesInEachFolder = new TreeMap<>();
+        mapOfListFilesInEachFolder = pathFinder.getMapOfListFilesInEachFolder(root, mapOfListFilesInEachFolder);
+        Map<String, List<String>> mapOfPairsImages = pathFinder.getMapOfPairsImages(mapOfListFilesInEachFolder);
+        pathFinder.saveMapOfListFilesInEachFolderToFile(mapOfListFilesInEachFolder);
+        pathFinder.saveMap(mapOfPairsImages);
     }
 }
--- src/PathFinder.java
+++ src/PathFinder.java
@@ -13,106 +13,126 @@ import java.util.*;
  */
 
 public class PathFinder {
-    private List<String> pathsAndNames;
+
     private List<String> paths;
     private List<String> names;
-    private List<String> namePairs;
+
     private final String tab = "\t";
     private File root;
 
-    public PathFinder(String rootPath) {
-        pathsAndNames = new ArrayList<>();
-        namePairs = new ArrayList<>();
+    public PathFinder(File root) {
         paths = new ArrayList<>();
         names = new ArrayList<>();
-        root = new File(rootPath);
+        this.root = root;
     }
 
-    public void findPathsAndNames(File root){
-        if(!root.isFile()){
-            if(root.getName().startsWith("S")){
-                File[] files = root.listFiles();
-                if(files != null){
-                    Map<String, List<String>> pairNames = new HashMap<>();
-                    for(File file : files){
-                        String nameStarts = file.getName().substring(0, 2);
-                        String path = file.getAbsolutePath();
-                        String name = file.getName();
-                        path = path.substring(0, path.length() - name.length());
-                        if(pairNames.containsKey(nameStarts)){
-                            pairNames.get(nameStarts).add(name + "%" + path);
-                        }else {
-                            List<String> pairNamesPaths = new ArrayList<>();
 
-                            pairNamesPaths.add(name + "%" + path);
-                            pairNames.put(nameStarts, pairNamesPaths);
-                        }
-                    }
-                    for(Map.Entry<String, List<String>> entry : pairNames.entrySet()){
-                        List<String> paths = entry.getValue();
-                        if(paths.size() > 1){
-                            namePairs.addAll(paths);
-                        }
-                    }
+    public Map<String, List<String>> getMapOfListFilesInEachFolder(File root, Map<String, List<String>> mapOfListFilesInEachFolder){
+        if(!root.isFile() && root.getName().startsWith("S")){
+            String imageFolder = root.getAbsolutePath();
+            File[] files = root.listFiles();
 
+            List<String> imagePaths = new ArrayList<>();
+            if(files != null){
+                if(files.length > 10) System.out.println(root.getAbsolutePath());
+                for(File file : files) {
+                    imagePaths.add(file.getName());
                 }
             }
-
-
-
-            File[] files = root.listFiles();
-            for(File file : files){
-                findPathsAndNames(file);
-            }
+            mapOfListFilesInEachFolder.put(imageFolder, imagePaths);
         }else {
-            String path = root.getAbsolutePath();
-            String name = root.getName();
-            name = NameCorrector.correct(name);
-            pathsAndNames.add(name + tab + path + "\r\n");
-        }
-    }
-
-    public void saveList(File root){
-        Path path = Paths.get(root.getAbsolutePath() + "/result.txt");
-        try {
-            Files.createFile(path);
-            PrintWriter printWriter = new PrintWriter(path.toFile().getAbsolutePath());
-            pathsAndNames.forEach(printWriter::print);
-        }catch (IOException e){
-            e.printStackTrace();
-        }
-    }
-
-
-
-    public void savePairsList(File root){
-        Stack<Character> stack = new Stack<>();
-        //6_Мозаика Corsa Deco Mist.jpg%X:\_project\Петрович\Таблички\2021\08 11 21 Панели Сириус\crop_pic\СЗФО\Выборг\S10\
-        namePairs.sort((o1, o2) -> {
-            String head1 = o1.substring(0, 2);
-            String tail1 = o1.split("%")[1];
-            String head2 = o2.substring(0, 2);
-            String tail2 = o2.split("%")[1];
-            if(head1.equals(head2) && tail1.equals(tail2)) return 0;
-            return 1;
-        });
-        for(String s : namePairs){
-            if(!s.equals("\r\n")){
-                System.out.println(s);
+            File[] files = root.listFiles();
+            if(files != null){
+                for(File file : files) {
+                    getMapOfListFilesInEachFolder(file, mapOfListFilesInEachFolder);
+                }
             }
-
         }
-        Path path = Paths.get(root.getAbsolutePath() + "/pairs.txt");
+        return mapOfListFilesInEachFolder;
+    }
+
+    public Map<String, List<String>> getMapOfPairsImages(Map<String, List<String>> mapOfListFilesInEachFolder) throws Exception{
+        Map<String, List<String>> mapOfPairsImages = new TreeMap<>();
+        for(Map.Entry<String, List<String>> entry : mapOfListFilesInEachFolder.entrySet()){
+            String folder = entry.getKey();
+            List<String> imagesInFolder = entry.getValue();
+            if(imagesInFolder.size()>1){
+                List<String> pairs = pairImages(imagesInFolder);
+                if(pairs.size() > 0 && pairs.size()%2 != 0) throw new Exception("pairs.size()%2 != 0");
+                if(pairs.size() >= 2){
+                    mapOfPairsImages.put(folder, pairs);
+                }
+
+            }
+        }
+        return mapOfPairsImages;
+    }
+
+    private List<String> pairImages(List<String> images){
+        List<String> pairs = new ArrayList<>();
+        Set<String> setStartNames = new HashSet<>();
+        for(String s : images) {
+            String nameStarts = s.substring(0, 2);
+            if(!setStartNames.add(nameStarts)){
+                for(String imageName : images){
+                    if(imageName.substring(0,2).equals(nameStarts)){
+                        pairs.add(tab + imageName);
+                    }
+                }
+            }
+        }
+        return pairs;
+    }
+
+    public void saveMap(Map<String, List<String>> mapOfPairsImages){
+        Path path = Paths.get(root.getAbsolutePath() + "/txt/pairImages.txt");
+        if(path.toFile().exists()){
+            path.toFile().delete();
+        }
         try {
             Files.createFile(path);
             PrintWriter printWriter = new PrintWriter(path.toFile().getAbsolutePath());
-            namePairs.forEach(printWriter::print);
+            for(Map.Entry<String, List<String>> entry : mapOfPairsImages.entrySet()){
+                String folder = entry.getKey();
+                List<String> imagesInFolder = entry.getValue();
+                printWriter.print(folder);
+                printWriter.print("\r\n");
+                for(String s : imagesInFolder){
+                    printWriter.print(s);
+                    printWriter.print("\r\n");
+                }
+                printWriter.print("\r\n");
+            }
+            printWriter.flush();
         }catch (IOException e){
             e.printStackTrace();
         }
     }
 
-    public File getRoot() {
-        return root;
+    public void saveMapOfListFilesInEachFolderToFile(Map<String, List<String>> mapOfListFilesInEachFolder){
+        Path path = Paths.get(root.getAbsolutePath() + "/txt/allFiles.txt");
+        if(path.toFile().exists()){
+            path.toFile().delete();
+        }
+        System.out.println("saving to" + path.toString() + " " + mapOfListFilesInEachFolder.values().size() + " strings");
+        try {
+            Files.createFile(path);
+            PrintWriter printWriter = new PrintWriter(path.toFile().getAbsolutePath());
+            for(Map.Entry<String, List<String>> entry : mapOfListFilesInEachFolder.entrySet()){
+                String folder = entry.getKey();
+                List<String> imagesInFolder = entry.getValue();
+                printWriter.print(folder);
+                printWriter.print("\r\n");
+                for(String s : imagesInFolder){
+                    printWriter.print(s);
+                    printWriter.print("\r\n");
+                }
+            }
+            printWriter.flush();
+        }catch (IOException e){
+            e.printStackTrace();
+        }
     }
+
+
 }
